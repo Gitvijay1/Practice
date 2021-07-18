38ccf5b2755ccfb302009d70b3b67df58a00aeea
Vijay Sharma <you@example.com>
2021-07-18 22:24:25 +0530

    third commit

---
+++ Demo.java
@@ -0,0 +1,7 @@
+public class Test{
+     public ststic void main(String[] args){
+            System.out.println("Ram");
+            System.out.println("Kumar");
+            System.out.println("Verma");
+                }
+          }
--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test{
      public ststic void main(String[] args){
             System.out.println("Hello");
             System.out.println("Hi");
+            System.out.println("Hi Hi");
                 }
           }
