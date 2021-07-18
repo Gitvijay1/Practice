f797d4267b74f6eee74bff24499602a12849ee99
Gitvijay1 <42443909+Gitvijay1@users.noreply.github.com>
2021-07-19 01:34:54 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,6 +2,7 @@ public class Test{
      public ststic void main(String[] args){
             System.out.println("Ram");
             System.out.println("Kumar");
-            System.out.println("Verma");
+            System.out.println("Hi Hi");
+             System.out.println("Hi Hi");
                 }
           }
